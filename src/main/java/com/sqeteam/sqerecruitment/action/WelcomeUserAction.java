f05c558e243d2e8555ac5752b0443758587d0225
rationalstudio <rationalstudio@gmail.com>
2017-04-07 03:22:59 +0530

    Struts

---
+++ src/main/java/com/sqeteam/sqerecruitment/action/WelcomeUserAction.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sqeteam.sqerecruitment.action;
+
+/**
+ *
+ * @author Dinuzhka
+ */
+public class WelcomeUserAction {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // all struts logic here
+    public String execute() {
+
+        return "SUCCESS";
+
+    }
+}
---
+++ src/main/webapp/User/pages/login.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : newjsplogin
+    Created on : Apr 7, 2017, 3:20:16 AM
+    Author     : Dinuzhka
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Login Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
